3f16b5a9205d8fe9bb894b3fae787b4c4535b14b
MSagGik <108148690+MSagGik@users.noreply.github.com>
2022-08-01 10:36:14 +0300

    Update ExampleLambdaExpressionReturn.java

--- src/ExampleLambdaExpressionReturn.java
+++ src/ExampleLambdaExpressionReturn.java
@@ -1,9 +1,9 @@
 interface Executable {
-    int execute(int x);
+    int execute(int x, int y);
 }
 class Runner {
     public void run(Executable e) {
-        int a = e.execute(10);
+        int a = e.execute(10, 15);
         System.out.println(a);
     }
 }
@@ -12,16 +12,15 @@ class Runner {
 public class ExampleLambdaExpressionReturn {
     public static void main(String[] args) {
         Runner runner = new Runner();
-        // реализация метода интерфейса Executable с возвращаемым значением:
+        // реализации метода интерфейса Executable с возвращаемым значением:
         // 1) с помощью анонимного класса
         runner.run(new Executable() {
             @Override
-            public int execute(int  x) {
-                return x+7;
+            public int execute(int  x, int y) {
+                return x+y;
             }
         });
         // 2) с помощью лямбда выражения
-        runner.run(x -> x+7);
+        runner.run((x,y) -> x+y);
     }
 }
-
